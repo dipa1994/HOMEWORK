5c4208848344dd4541fdb05e50d8021e1be54ba3
Dipa <dipa.smvs@gmail.com>
2022-04-12 20:44:04 +0100

    first

---
+++ EligibleForVoting.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class EligibleForVoting {
+    public static void main(String[] args) {
+        int age = 17;
+            System.out.println("Enter your age = ");
+        if (age>=18) {
+            System.out.println("Congratulation, You are Eligible for Vote");
+        }
+        else
+        {
+            System.out.println("Sorry, You are not Eligible for Vote");
+
+        }
+    }
+}
---
+++ PosNegNumbers.java
@@ -0,0 +1,21 @@
+package Homework;
+
+public class PosNegNumbers {
+
+    public static void main(String[] args) {
+        int Number = 0;
+
+        if (Number > 0) {
+            System.out.println("The Number is Positive");
+        }
+        else if (Number < 0)
+        {
+            System.out.println("The Number is Negative");
+        }
+        else
+        {
+            System.out.println("The Number is Zero");
+        }
+
+        }
+    }
